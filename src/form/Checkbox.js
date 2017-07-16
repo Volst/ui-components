5fb2d8dadcbf5ea7aac8649b6ea39a9c99961b6f
Kees Kluskens <kees@webduck.nl>
2017-07-16 18:18:47 +0200

    Add Checkbox component

---
+++ src/form/Checkbox.js
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledLabel = styled.label`
+    width: 100%;
+    display: block;
+    margin-bottom: 3px;
+    cursor: pointer;
+`;
+
+const StyledInput = styled.input`
+    margin-right: 5px;
+    position: relative;
+    top: -1px;
+`;
+
+export default class Checkbox extends Component {
+    static propTypes = {
+        onChange: PropTypes.func.isRequired,
+        name: PropTypes.string,
+        label: PropTypes.string,
+        value: PropTypes.bool,
+        disabled: PropTypes.bool,
+    };
+
+    handleChange = e => {
+        this.props.onChange(this.props.name, e.target.checked);
+    };
+
+    render() {
+        return (
+            <StyledLabel>
+                <StyledInput
+                    type="checkbox"
+                    onChange={this.handleChange}
+                    checked={this.props.value}
+                    disabled={this.props.disabled}
+                />
+                {this.props.label}
+            </StyledLabel>
+        );
+    }
+}
---
+++ src/form/Checkbox.stories.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { storiesOf, action } from '@storybook/react';
+import Checkbox from './Checkbox';
+import CenterDecorator from '../../storybook/CenterDecorator';
+
+storiesOf('Checkbox', module)
+    .addDecorator(CenterDecorator)
+    .addWithInfo('standard', () => {
+        return (
+            <form>
+                <Checkbox
+                    onChange={action('change')}
+                    label="Zebra"
+                    name="animals"
+                    value={true}
+                />
+                <Checkbox
+                    onChange={action('change')}
+                    name="animals"
+                    label="Lion"
+                    value={false}
+                />
+            </form>
+        );
+    })
+    .add('disabled', () => {
+        return (
+            <Checkbox
+                disabled
+                onChange={action('change')}
+                name="animals"
+                label="Zebra"
+                value="zebra"
+            />
+        );
+    });
