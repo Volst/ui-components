abcf1e3158a2f0538113bf0cdaeffbc8b26c1ffb
Kees Kluskens <kees@webduck.nl>
2017-07-18 11:14:07 +0200

    export Checkbox component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as ReCyCleTheme } from './ReCyCleTheme';
 export { default as RadioButtons } from './form/RadioButtons';
+export { default as Checkbox } from './form/Checkbox';
 export { default as Modal } from './Modal';
