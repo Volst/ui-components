c2b1cf621b96a7c0b8e0d3b8501ee9f560b0e92c
Kees Kluskens <kees@webduck.nl>
2017-07-18 10:53:07 +0200

    0.1.1

---
+++ dist/re-cy-cle.es.js
@@ -0,0 +1,250 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled, { ThemeProvider } from 'styled-components';
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
+
+var _class;
+var _descriptor;
+var _class2;
+var _temp2;
+
+function _initDefineProp(target, property, descriptor, context) {
+    if (!descriptor) return;
+    Object.defineProperty(target, property, {
+        enumerable: descriptor.enumerable,
+        configurable: descriptor.configurable,
+        writable: descriptor.writable,
+        value: descriptor.initializer
+            ? descriptor.initializer.call(context)
+            : void 0,
+    });
+}
+
+function _applyDecoratedDescriptor(
+    target,
+    property,
+    decorators,
+    descriptor,
+    context
+) {
+    var desc = {};
+    Object['ke' + 'ys'](descriptor).forEach(function(key) {
+        desc[key] = descriptor[key];
+    });
+    desc.enumerable = !!desc.enumerable;
+    desc.configurable = !!desc.configurable;
+
+    if ('value' in desc || desc.initializer) {
+        desc.writable = true;
+    }
+
+    desc = decorators.slice().reverse().reduce(function(desc, decorator) {
+        return decorator(target, property, desc) || desc;
+    }, desc);
+
+    if (context && desc.initializer !== void 0) {
+        desc.value = desc.initializer ? desc.initializer.call(context) : void 0;
+        desc.initializer = undefined;
+    }
+
+    if (desc.initializer === void 0) {
+        Object['define' + 'Property'](target, property, desc);
+        desc = null;
+    }
+
+    return desc;
+}
+
+const StyledDiv = styled.div.withConfig({
+    displayName: 'RadioButtons__StyledDiv',
+})(
+    [
+        'display:flex;align-items:stretch;border:1px solid transparent;border-radius:4px;',
+        ';',
+    ],
+    props =>
+        props.focus &&
+        `
+        border-color: ${props => props.theme.primary};
+    `
+);
+
+const Option = styled.div.withConfig({
+    displayName: 'RadioButtons__Option',
+})([
+    'flex:1;display:flex;justify-content:center;&:first-child > label{border-top-left-radius:4px;border-bottom-left-radius:4px;border-left-width:1px;}&:last-child > label{border-top-right-radius:4px;border-bottom-right-radius:4px;}',
+]);
+
+const StyledLabel = styled.label.withConfig({
+    displayName: 'RadioButtons__StyledLabel',
+})(
+    [
+        'flex:1;cursor:',
+        ';display:flex;align-items:center;justify-content:center;padding:6px;text-align:center;border:1px solid #ccc;border-left-width:0;background:#fff;font-size:14px;color:rgba(0,0,0,0.5);',
+    ],
+    props => (props.disabled ? 'not-allowed' : 'pointer')
+);
+
+const StyledInput = styled.input.withConfig({
+    displayName: 'RadioButtons__StyledInput',
+})(
+    [
+        'position:fixed;left:-999999px;opacity:0;&:checked + label{background:',
+        ';border-color:',
+        ';color:#fff;box-shadow:-1px 0 ',
+        ';}',
+    ],
+    props => props.theme.primary,
+    props => props.theme.primary,
+    props => props.theme.primary
+);
+
+var RadioButtons = observer(
+    (
+        (_class = (
+            (_temp2 = _class2 = class RadioButtons extends Component {
+                constructor(...args) {
+                    var _temp;
+
+                    return (_temp = super(...args)), _initDefineProp(
+                        this,
+                        'hasFocus',
+                        _descriptor,
+                        this
+                    ), (this.handleChange = value => {
+                        if (!this.props.disabled) {
+                            this.props.onChange(this.props.name, value);
+                        }
+                    }), (this.renderItem = item => {
+                        const handleChange = () =>
+                            this.handleChange(item.value);
+                        return React.createElement(
+                            Option,
+                            { key: item.value },
+                            React.createElement(StyledInput, {
+                                tabIndex: '0',
+                                type: 'radio',
+                                name: this.props.name,
+                                checked: item.value === this.props.value,
+                                onChange: handleChange,
+                                disabled: this.props.disabled,
+                            }),
+                            React.createElement(
+                                StyledLabel,
+                                {
+                                    onClick: handleChange,
+                                    disabled: this.props.disabled,
+                                },
+                                item.label
+                            )
+                        );
+                    }), (this.handleFocus = () => {
+                        this.hasFocus = true;
+                    }), (this.handleBlur = () => {
+                        this.hasFocus = false;
+                    }), _temp;
+                }
+
+                render() {
+                    return React.createElement(
+                        StyledDiv,
+                        {
+                            onFocus: this.handleFocus,
+                            onBlur: this.handleBlur,
+                            focus: this.hasFocus,
+                        },
+                        this.props.options.map(this.renderItem)
+                    );
+                }
+            }),
+            (_class2.propTypes = {
+                onChange: PropTypes.func,
+                name: PropTypes.string,
+                disabled: PropTypes.bool,
+                options: PropTypes.array.isRequired,
+                value: PropTypes.oneOfType([
+                    PropTypes.bool,
+                    PropTypes.string,
+                    PropTypes.number,
+                ]),
+            }),
+            _temp2
+        )),
+        (_descriptor = _applyDecoratedDescriptor(
+            _class.prototype,
+            'hasFocus',
+            [observable],
+            {
+                enumerable: true,
+                initializer: function() {
+                    return false;
+                },
+            }
+        )),
+        _class
+    )
+);
+
+var _class$1;
+var _temp2$1;
+
+const Container = styled.div.withConfig({
+    displayName: 'Modal__Container',
+})([
+    'position:fixed;top:0;bottom:0;left:0;right:0;display:flex;align-items:center;justify-content:center;',
+]);
+
+const Background = styled.div.withConfig({
+    displayName: 'Modal__Background',
+})([
+    'position:absolute;background:rgba(0,0,0,0.5);width:100%;height:100%;cursor:pointer;',
+]);
+
+const Content = styled.div.withConfig({
+    displayName: 'Modal__Content',
+})([
+    'position:relative;background:#fff;border-radius:4px;display:flex;overflow:hidden;height:80vh;width:80vw;max-width:800px;max-height:800px;',
+]);
+
+const ESCAPE_KEY = 27;
+
+let Modal = (
+    (_temp2$1 = _class$1 = class Modal extends Component {
+        constructor(...args) {
+            var _temp;
+
+            return (_temp = super(...args)), (this.handleKeyDown = e => {
+                if (e.keyCode === ESCAPE_KEY) {
+                    this.props.onClose();
+                }
+            }), _temp;
+        }
+
+        componentWillMount() {
+            document.addEventListener('keydown', this.handleKeyDown);
+        }
+
+        componentWillUnmount() {
+            document.removeEventListener('keydown', this.handleKeyDown);
+        }
+
+        render() {
+            return React.createElement(
+                Container,
+                null,
+                React.createElement(Background, {
+                    onClick: this.props.onClose,
+                }),
+                React.createElement(Content, null, this.props.children)
+            );
+        }
+    }),
+    (_class$1.propTypes = {
+        children: PropTypes.node.isRequired,
+        onClose: PropTypes.func.isRequired,
+    }),
+    _temp2$1
+);
+
+export { ThemeProvider as ReCyCleTheme, RadioButtons, Modal };
---
+++ dist/re-cy-cle.umd.js
@@ -0,0 +1,295 @@
+(function(global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined'
+        ? factory(
+              exports,
+              require('react'),
+              require('prop-types'),
+              require('styled-components'),
+              require('mobx-react'),
+              require('mobx')
+          )
+        : typeof define === 'function' && define.amd
+          ? define(
+                [
+                    'exports',
+                    'react',
+                    'prop-types',
+                    'styled-components',
+                    'mobx-react',
+                    'mobx',
+                ],
+                factory
+            )
+          : factory(
+                (global.reCyCle = {}),
+                global.React,
+                global.PropTypes,
+                global.styled,
+                global.mobxReact,
+                global.mobx
+            );
+})(this, function(exports, React, PropTypes, styled, mobxReact, mobx) {
+    'use strict';
+
+    var React__default = 'default' in React ? React['default'] : React;
+    PropTypes =
+        PropTypes && PropTypes.hasOwnProperty('default')
+            ? PropTypes['default']
+            : PropTypes;
+    var styled__default = 'default' in styled ? styled['default'] : styled;
+
+    var _class;
+    var _descriptor;
+    var _class2;
+    var _temp2;
+
+    function _initDefineProp(target, property, descriptor, context) {
+        if (!descriptor) return;
+        Object.defineProperty(target, property, {
+            enumerable: descriptor.enumerable,
+            configurable: descriptor.configurable,
+            writable: descriptor.writable,
+            value: descriptor.initializer
+                ? descriptor.initializer.call(context)
+                : void 0,
+        });
+    }
+
+    function _applyDecoratedDescriptor(
+        target,
+        property,
+        decorators,
+        descriptor,
+        context
+    ) {
+        var desc = {};
+        Object['ke' + 'ys'](descriptor).forEach(function(key) {
+            desc[key] = descriptor[key];
+        });
+        desc.enumerable = !!desc.enumerable;
+        desc.configurable = !!desc.configurable;
+
+        if ('value' in desc || desc.initializer) {
+            desc.writable = true;
+        }
+
+        desc = decorators.slice().reverse().reduce(function(desc, decorator) {
+            return decorator(target, property, desc) || desc;
+        }, desc);
+
+        if (context && desc.initializer !== void 0) {
+            desc.value = desc.initializer
+                ? desc.initializer.call(context)
+                : void 0;
+            desc.initializer = undefined;
+        }
+
+        if (desc.initializer === void 0) {
+            Object['define' + 'Property'](target, property, desc);
+            desc = null;
+        }
+
+        return desc;
+    }
+
+    const StyledDiv = styled__default.div.withConfig({
+        displayName: 'RadioButtons__StyledDiv',
+    })(
+        [
+            'display:flex;align-items:stretch;border:1px solid transparent;border-radius:4px;',
+            ';',
+        ],
+        props =>
+            props.focus &&
+            `
+        border-color: ${props => props.theme.primary};
+    `
+    );
+
+    const Option = styled__default.div.withConfig({
+        displayName: 'RadioButtons__Option',
+    })([
+        'flex:1;display:flex;justify-content:center;&:first-child > label{border-top-left-radius:4px;border-bottom-left-radius:4px;border-left-width:1px;}&:last-child > label{border-top-right-radius:4px;border-bottom-right-radius:4px;}',
+    ]);
+
+    const StyledLabel = styled__default.label.withConfig({
+        displayName: 'RadioButtons__StyledLabel',
+    })(
+        [
+            'flex:1;cursor:',
+            ';display:flex;align-items:center;justify-content:center;padding:6px;text-align:center;border:1px solid #ccc;border-left-width:0;background:#fff;font-size:14px;color:rgba(0,0,0,0.5);',
+        ],
+        props => (props.disabled ? 'not-allowed' : 'pointer')
+    );
+
+    const StyledInput = styled__default.input.withConfig({
+        displayName: 'RadioButtons__StyledInput',
+    })(
+        [
+            'position:fixed;left:-999999px;opacity:0;&:checked + label{background:',
+            ';border-color:',
+            ';color:#fff;box-shadow:-1px 0 ',
+            ';}',
+        ],
+        props => props.theme.primary,
+        props => props.theme.primary,
+        props => props.theme.primary
+    );
+
+    var RadioButtons = mobxReact.observer(
+        (
+            (_class = (
+                (_temp2 = _class2 = class RadioButtons extends React.Component {
+                    constructor(...args) {
+                        var _temp;
+
+                        return (_temp = super(...args)), _initDefineProp(
+                            this,
+                            'hasFocus',
+                            _descriptor,
+                            this
+                        ), (this.handleChange = value => {
+                            if (!this.props.disabled) {
+                                this.props.onChange(this.props.name, value);
+                            }
+                        }), (this.renderItem = item => {
+                            const handleChange = () =>
+                                this.handleChange(item.value);
+                            return React__default.createElement(
+                                Option,
+                                { key: item.value },
+                                React__default.createElement(StyledInput, {
+                                    tabIndex: '0',
+                                    type: 'radio',
+                                    name: this.props.name,
+                                    checked: item.value === this.props.value,
+                                    onChange: handleChange,
+                                    disabled: this.props.disabled,
+                                }),
+                                React__default.createElement(
+                                    StyledLabel,
+                                    {
+                                        onClick: handleChange,
+                                        disabled: this.props.disabled,
+                                    },
+                                    item.label
+                                )
+                            );
+                        }), (this.handleFocus = () => {
+                            this.hasFocus = true;
+                        }), (this.handleBlur = () => {
+                            this.hasFocus = false;
+                        }), _temp;
+                    }
+
+                    render() {
+                        return React__default.createElement(
+                            StyledDiv,
+                            {
+                                onFocus: this.handleFocus,
+                                onBlur: this.handleBlur,
+                                focus: this.hasFocus,
+                            },
+                            this.props.options.map(this.renderItem)
+                        );
+                    }
+                }),
+                (_class2.propTypes = {
+                    onChange: PropTypes.func,
+                    name: PropTypes.string,
+                    disabled: PropTypes.bool,
+                    options: PropTypes.array.isRequired,
+                    value: PropTypes.oneOfType([
+                        PropTypes.bool,
+                        PropTypes.string,
+                        PropTypes.number,
+                    ]),
+                }),
+                _temp2
+            )),
+            (_descriptor = _applyDecoratedDescriptor(
+                _class.prototype,
+                'hasFocus',
+                [mobx.observable],
+                {
+                    enumerable: true,
+                    initializer: function() {
+                        return false;
+                    },
+                }
+            )),
+            _class
+        )
+    );
+
+    var _class$1;
+    var _temp2$1;
+
+    const Container = styled__default.div.withConfig({
+        displayName: 'Modal__Container',
+    })([
+        'position:fixed;top:0;bottom:0;left:0;right:0;display:flex;align-items:center;justify-content:center;',
+    ]);
+
+    const Background = styled__default.div.withConfig({
+        displayName: 'Modal__Background',
+    })([
+        'position:absolute;background:rgba(0,0,0,0.5);width:100%;height:100%;cursor:pointer;',
+    ]);
+
+    const Content = styled__default.div.withConfig({
+        displayName: 'Modal__Content',
+    })([
+        'position:relative;background:#fff;border-radius:4px;display:flex;overflow:hidden;height:80vh;width:80vw;max-width:800px;max-height:800px;',
+    ]);
+
+    const ESCAPE_KEY = 27;
+
+    let Modal = (
+        (_temp2$1 = _class$1 = class Modal extends React.Component {
+            constructor(...args) {
+                var _temp;
+
+                return (_temp = super(...args)), (this.handleKeyDown = e => {
+                    if (e.keyCode === ESCAPE_KEY) {
+                        this.props.onClose();
+                    }
+                }), _temp;
+            }
+
+            componentWillMount() {
+                document.addEventListener('keydown', this.handleKeyDown);
+            }
+
+            componentWillUnmount() {
+                document.removeEventListener('keydown', this.handleKeyDown);
+            }
+
+            render() {
+                return React__default.createElement(
+                    Container,
+                    null,
+                    React__default.createElement(Background, {
+                        onClick: this.props.onClose,
+                    }),
+                    React__default.createElement(
+                        Content,
+                        null,
+                        this.props.children
+                    )
+                );
+            }
+        }),
+        (_class$1.propTypes = {
+            children: PropTypes.node.isRequired,
+            onClose: PropTypes.func.isRequired,
+        }),
+        _temp2$1
+    );
+
+    exports.ReCyCleTheme = styled.ThemeProvider;
+    exports.RadioButtons = RadioButtons;
+    exports.Modal = Modal;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+});
