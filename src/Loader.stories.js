f00c6e1e0ba44cc9ccff37c0d0c2ff261c01fffd
Kees Kluskens <kees@webduck.nl>
2017-08-05 15:32:52 +0200

    Add stories file for Loader

---
+++ src/Loader.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import CenterDecorator from '../storybook/CenterDecorator';
+import Loader from './Loader';
+
+storiesOf('Loader', module).addDecorator(CenterDecorator).add(
+    'standard',
+    withInfo()(() => {
+        return <Loader isLoading />;
+    })
+);
