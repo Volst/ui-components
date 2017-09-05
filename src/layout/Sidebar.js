d262a18a28ac1413dfbbad1ba7dc35862cd4cc32
Rutger Schimmel <rma.schimmel@gmail.com>
2017-09-05 12:30:27 +0200

    Add Sidebar CSS transitions

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -4,8 +4,34 @@ import styled from 'styled-components';
 import { Scrollbars } from 'react-custom-scrollbars';
 
 const StyledAside = styled.aside`
-    width: ${props => (props.medium ? 450 : 350)}px;
-    background: #eee;
+    ${props => {
+        const width = props.medium ? 450 : 350;
+        return `
+            width: ${width}px;
+            background: #eee;
+
+            &.slide-right-enter,
+            &.slide-right-leave.slide-right-leave-active {
+                margin-right: -${width}px;
+            }
+            &.slide-left-enter,
+            &.slide-left-leave.slide-left-leave-active {
+                margin-left: -${width}px;
+            }
+            &.slide-right-leave,
+            &.slide-right-enter.slide-right-enter-active,
+            &.slide-left-leave,
+            &.slide-left-enter.slide-left-enter-active {
+                margin-right: 0;
+            }
+            &.slide-right-enter-active,
+            &.slide-right-leave-active,
+            &.slide-left-enter-active,
+            &.slide-left-leave-active {
+                transition: margin 300ms ease;
+            }
+        `;
+    }};
 `;
 
 const Content = styled.div`padding: 25px;`;
