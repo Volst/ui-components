8767ec3afd7f32d3404e11921841cdc9c53b63ed
Rutger Schimmel <rma.schimmel@gmail.com>
2018-01-11 15:27:42 +0100

    Heading: fix alignment issue

--- src/general/typography/Heading.js
+++ src/general/typography/Heading.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Heading = styled.h1`
   font-weight: bold;
   font-size: 26px;
-  margin: 20px 0 7px 0;
+  margin: 20px 0;
   color: ${props => props.color || props.theme.headingTextColor};
 `;
 Heading.displayName = 'Heading';
