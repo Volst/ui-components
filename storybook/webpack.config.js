a35d6dfb168ca1b545f43bb7cb005b399aa42715
Kees Kluskens <kees@webduck.nl>
2018-02-04 22:03:53 +0100

    Improve Storybook build perf

--- storybook/webpack.config.js
+++ storybook/webpack.config.js
@@ -1,4 +1,9 @@
 const webpack = require('webpack');
+const path = require('path');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+const appSrc = path.resolve(__dirname, '../src');
+const storybookSrc = path.resolve(__dirname, '../storybook');
 
 // TODO: This whole file can be removed if PR https://github.com/storybooks/storybook/pull/1733 is merged & released.
 
@@ -13,12 +18,24 @@ module.exports = (baseConfig, configType) => {
   }
   baseConfig.module.rules.push({
     test: /\.(ts|tsx)$/,
+    include: [appSrc, storybookSrc],
     loader: require.resolve('ts-loader'),
+    options: {
+      // disable type checker - we will use it in fork plugin
+      transpileOnly: true,
+    },
   });
   baseConfig.resolve.extensions.push('.ts', '.tsx');
   baseConfig.module.rules.push({
     test: /\.(woff2)$/,
     loader: 'file-loader',
   });
+  baseConfig.plugins.push(
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      watch: [appSrc, storybookSrc],
+      tsconfig: path.resolve(__dirname, '../tsconfig.json'),
+    })
+  );
   return baseConfig;
 };
